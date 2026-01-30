fe6b63238135124d42c0436d8fe09fcc70e5cbcd
deliwalayash <deliwalayash2@gmail.com>
2026-01-30 17:40:28 +0530

    some changes made

--- react-router-context/src/pages/Dashboard.tsx
+++ react-router-context/src/pages/Dashboard.tsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
 
   return (
     <div className="p-8">
-      <h1 className="text-3xl font-bold">Dashboard</h1>
+      <h1 className="text-3xl font-bold">Home Dashboard</h1>
       <p className="mt-2">
         Authenticated: {isAuthenticated ? "Yes" : "No"}
       </p>
